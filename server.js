cb8b0d04d083977f9819a5065bc946a795e9b93b
lyhuynhnhu <lyhuynhnhu200699@gmail.com>
2019-06-26 15:54:49 +0700

    update scss

--- server.js
+++ server.js
@@ -1,29 +1,36 @@
-var http= require('http');
-var fs= require('fs');
-var url=require('url');
-var path=require('path');
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = process.argv[2] || 8888;
 
-//create a server
-http.createServer( function(req, res){
-    var pathname= url.parse(req.url,true);
-    var filename= "index.html" + pathname.pathname;
-    //print file's name
-    // console.log("request for "+ pathname +" received");
+http.createServer(function(request, response) {
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
 
-    fs.readFile(filename, function (err, data) {
-        console.log(pathname)
-        if (err) {
-           //console.log(err);
-           res.writeHead(404, {'Content-Type': 'text/html'});
-           res.end('<h1>404 not found</h1>');
-        } else {	
-           res.writeHead(200, {'Content-Type': 'text/html'});
-        //    res.writeHead(200, {'Content-Type': 'text/css'});	
-           res.write(data);	
-           res.end();	
-        }
-        
-    });      
-}).listen(8080);
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
 
-console.log('running at http://127.0.0.1:8080/');
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
